f8970199871ed81122ed9f35acb944d9affd7fd4
naiknetra <naiknetra5@gmail.com>
2024-02-24 21:04:04 -0500

    updated Javascript

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  var passwordLength = window.prompt("What is the length of the password? it should be between 8 to 128 ");
+  if (passwordLength < 8  || passwordLength > 128) {
+    window.alert("Invalid entry")
+    return""
+  }
 
+  var lowercase = window.confirm("Do you want to include lowercase?");
+  var uppercase = window.confirm("Do you want to include uppercase?");
+  var numeric = window.confirm("Do you want to include numeric?");
+  var specialchar = window.confirm("Do you want to include Special character?");
+
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
